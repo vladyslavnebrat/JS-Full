1e5d6d947d03d4bfa288c8883504a1a5183fc7d1
Vladyslav Nebrat <vladyslav.nebrat@gmail.com>
2021-12-13 18:44:37 +0200

    add

---
+++ Javascript intro/index.js
@@ -0,0 +1,8 @@
+function run() {
+  var a = 0;
+    var b = 1;
+
+  if (a == b) {
+    return c;
+  }
+}
---
+++ Operators/Using operators/Type casting/index.js
@@ -0,0 +1,30 @@
+// code below must be finished ;)
+
+/* cast to string */
+console.log('17 to string is ' + String(17));
+console.log('-17.17 to string is ');
+console.log('false to string is ' + String(-17.17));
+console.log('null to string is ' + String(null));
+console.log('undefined to string is ' + String(undefined));
+console.log('0 to string is ' + String(0));
+
+/* cast to number */
+console.log("'17' to number is " + Number('17'));
+console.log('true to number is ' + Number(true));
+console.log('false to number is ' + Number(false));
+console.log('null to number is ' + Number(null));
+console.log('undefined to number is ' + Number(undefined));
+console.log("'   20   ' to number is " + Number('   20   '));
+console.log("'      ' to number is " + Number('      '));
+console.log("'   30d   ' to number is " + Number('   30d   '));
+
+/* cast to boolean */
+console.log('null to boolean is ');
+console.log('undefined to boolean is ');
+console.log('0 to boolean is ');
+console.log('-0 to boolean is ');
+console.log('NaN to boolean is ');
+console.log("'' to boolean is ");
+console.log("' ' to boolean is ");
+console.log('17 to boolean is ');
+console.log("'Hello' to boolean is ");
