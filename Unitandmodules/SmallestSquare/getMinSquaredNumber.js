14069be181d5a1b529c0901af941ce98caa57023
Vladyslav Nebrat <vladyslav.nebrat@gmail.com>
2022-05-09 21:07:10 +0300

    add

---
+++ Unitandmodules/SmallestSquare/getMinSquaredNumber.js
@@ -0,0 +1,16 @@
+const minSquaredNumber = (arr) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return null;
+  }
+  const squaredArray = arr.forEach((element) => {
+    Math.abs(element * element);
+  });
+  let min = squaredArray[0];
+  let i = 0;
+  for (i = 1; i < squaredArray.length; i++) {
+    if (squaredArray[i] < min) {
+      min = squaredArray[i];
+    }
+  }
+  return min;
+};
